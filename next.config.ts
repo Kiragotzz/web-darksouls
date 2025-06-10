0557382875b9b5fe21e4f7c5a06911feeca252b1
Kiragotzz <luis_gabriell1@hotmail.com>
2025-06-10 20:55:19 -0300

    fetching data from api-darksouls

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  output: "export",
   /* config options here */
   experimental: {
     //appDir: true, // Fundamental para App Router
